0aa8b788ea121368d304e144f0c914bf9de4b156
Matthias Bittner <matthias.bittner@pls-europe.eu>
2017-05-22 09:17:01 +0200

    ...

--- scripts/browser_chrome.bat
+++ scripts/browser_chrome.bat
@@ -1,6 +1,6 @@
 @echo Off
-REM 64bit
-wmic datafile where name="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" get Version /value
 REM 32bit
 wmic datafile where name="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" get Version /value
+REM 64bit
+wmic datafile where name="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" get Version /value
 
