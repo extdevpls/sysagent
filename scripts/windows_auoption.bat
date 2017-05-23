bacff7ca51b73719527d19d3f783acb68d6a9dd5
Matthias Bittner <matthias.bittner@pls-europe.eu>
2017-05-23 08:01:18 +0200

    ...

--- scripts/windows_auoption.bat
+++ scripts/windows_auoption.bat
@@ -1,2 +1,5 @@
 @echo off
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v NoAutoUpdate
 reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v ScheduledInstallDay
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v ScheduledInstallTime
